dba1eeb9f6607f5f4e9da8eb70b6e0c569c1892a
Guillaume Caussarieu <guillaume.caussarieu@gmail.com>
2023-06-02 13:26:21 +0200

    CLT Implementation Start

---
+++ StructuralDesignKitLibrary/CrossSections/CLT_Layup.cs
@@ -0,0 +1,74 @@
+﻿using StructuralDesignKitLibrary.Materials;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructuralDesignKitLibrary.CrossSections
+{
+	/// <summary>
+	/// Class representing the composition of a CLT layup
+	/// </summary>
+	public class CLT_Layup
+	{
+		
+		/// <summary>
+		/// List of lamella thicknesses
+		/// </summary>
+		public List<int> Thicknesses { get; set; }
+
+		/// <summary>
+		/// List of lamella orientation (0° or 90°)
+		/// </summary>
+		private List<int> _orientations;
+
+		public List<int> Orientations
+		{
+			get { return _orientations; }
+			set
+			{
+				//Verify if the lamella orientation is properly defined
+				foreach (int angle in value)
+				{
+					if (angle != 0 || angle != 90) throw new Exception("The CLT lamella angle can only be 0° or 90°");
+				}
+				_orientations = value;
+			}
+		}
+
+		/// <summary>
+		/// List of the material constituing each lamella
+		/// </summary>
+		public List<IMaterial> Materials { get; set; }
+
+		/// <summary>
+		/// define the with of a single lamella
+		/// </summary>
+		public int LamellaWidth { get; set; }
+
+		/// <summary>
+		/// define if the narrow side of the lamella is glued
+		/// </summary>
+		public bool NarrowSideGlued { get; set; }
+
+
+        public CLT_Layup(int lamellaWidth = 150, bool narrowSideGlued = false)
+        {
+			Thicknesses = new List<int>();
+			_orientations = new List<int>();
+			Materials = new List<IMaterial>();
+			LamellaWidth = lamellaWidth;
+			NarrowSideGlued	= narrowSideGlued;
+        }
+
+		public CLT_Layup(List<int>thicknesses, List<int>orientations, List<IMaterial> materials,int lamellaWidth = 150, bool narrowSideGlued = false)
+		{
+			Thicknesses = thicknesses;
+			_orientations = new List<int>();
+			Materials = new List<IMaterial>();
+			LamellaWidth = lamellaWidth;
+			NarrowSideGlued = narrowSideGlued;
+		}
+	}
+}
--- StructuralDesignKitLibrary/CrossSections/CrossSectionCLT.cs
+++ StructuralDesignKitLibrary/CrossSections/CrossSectionCLT.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Dlubal.RFEM5;
+using StructuralDesignKitLibrary.CrossSections.Interfaces;
+using StructuralDesignKitLibrary.Materials;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,74 @@ using System.Threading.Tasks;
 
 namespace StructuralDesignKitLibrary.CrossSections
 {
-	internal class CrossSectionCLT
+	public class CrossSectionCLT
 	{
+		public int ID { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public int Thickness { get; set; }
+		public CLT_Layup Layup { get; set; }
+		public double AreaX { get; set; }
+		public double AreaY { get; set; }
+		public double MomentOfInertia_XX { get; set; }
+		public double MomentOfInertia_YY { get; set; }
+		public double TorsionalInertia { get; set; }
+		public double SectionModulus_XX { get; set; }
+		public double SectionModulus_YY { get; set; }
+		public double TorsionalModulus { get; set; }
+		public double NxCompressionChar { get; set; }
+		public double NxTensionChar { get; set; }
+		public double NyCompressionChar { get; set; }
+		public double NyTensionChar { get; set; }
+		public double NxyChar { get; set; }
+		public double MxChar { get; set; }
+		public double MxyChar { get; set; }
+		public double VxChar { get; set; }
+		public double VyChar { get; set; }
+
+		//------------------------------------------------------------------------------------
+		//Define EIx EIy, GAx, GAy, ExEquivalent, EyEquivalent to be defined as orthotropic material in Karamba for instance 
+		//with the constant stiffness representing the overall thickness of the CLT plate
+		//------------------------------------------------------------------------------------
+
+
+		//Implement a picture in Excel / GH equivalent to KHL documentation
+
+
+
+		public void ComputeCrossSectionProperties()
+		{
+			throw new NotImplementedException();
+		}
+
+		public List<double> ComputeNormalStress(double NormalForce)
+		{
+			throw new NotImplementedException();
+		}
+
+		public List<double> ComputeShearStressX(double ShearForceY)
+		{
+			throw new NotImplementedException();
+		}
+
+		public List<double> ComputeShearStressY(double ShearForceZ)
+		{
+			throw new NotImplementedException();
+		}
+
+		public List<double> ComputeStressBendingX(double BendingMomentY)
+		{
+			throw new NotImplementedException();
+		}
+
+		public List<double> ComputeStressBendingY(double BendingMomentZ)
+		{
+			throw new NotImplementedException();
+		}
+
+		public List<double> ComputeTorsionStress(double TorsionMoment)
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
